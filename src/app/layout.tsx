894ec3214a5d9fc86dfa201dc31c2775e37e1917
George B.K <george140201@gmail.com>
2025-06-05 07:30:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
         <meta name="theme-color" content="#111827" /> {/* Theme color for PWA */}
-        {/* Removed duplicate manifest link, it's now in Metadata object */}
       </head>
       <body className="font-body antialiased bg-background text-foreground min-h-screen flex flex-col">
         <InventoryProvider>
