57a934be4d3ca7067f901f42a41d291a699d5d9d
George B.K <george140201@gmail.com>
2025-07-06 08:18:22 +0000

    SW not found

--- src/components/ServiceWorkerRegistration.tsx
+++ src/components/ServiceWorkerRegistration.tsx
@@ -4,15 +4,17 @@ import { useEffect } from 'react';
 
 const ServiceWorkerRegistration = () => {
   useEffect(() => {
-    if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then((registration) => {
-          console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch((error) => {
-          console.error('Service Worker registration failed:', error);
-        });
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker
+          .register('/sw.js')
+          .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+          })
+          .catch((error) => {
+            console.error('Service Worker registration failed:', error);
+          });
+      });
     }
   }, []);
 
